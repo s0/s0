a12a789eb523bb3518821033e97c809b58de8cb9
Sam Lanning <sam@lanni.ng>
2019-07-20 16:08:04 -0700

    Initial react component

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import {Home} from './components/pages/home';
+
+ReactDOM.render(<Home />, document.getElementById('stage'));
