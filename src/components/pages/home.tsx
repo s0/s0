53a37e16b97d73d1f625ec0fa3cc07061bf048ad
Sam Lanning <sam@samlanning.com>
2019-08-15 15:44:22 -0700

    Reorder stuff

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -86,48 +86,48 @@ class Home extends React.Component<Props, {}> {
                 </ul>
               </div>
               <div className="section">
-                <h2>Career</h2>
-                <ul className="bolded">
-                  <li>
-                    <strong>2011 - 2014</strong> - Computer Science Bachelor's Degree, University of Oxford.
-                  </li>
-                  <li>
-                    <strong>Feb 2012 - Feb 2014</strong> - Spent 1 year as Secretary, and 1 year as President of the Oxford University Computer Society
-                  </li>
-                  <li>
-                    <strong>Nov 2014 - Jul 2015</strong> - Computer Science Specialist Tutor @ <a href="https://www.lvs-oxford.org.uk/" target="_blank">LVS Oxford</a> - a school for young people / children on the autistic spectrum
-                  </li>
-                  <li>
-                    <strong>Oct 2014 - Mar 2018</strong> - Software Engineer @ <a href="https://semmle.com" target="_blank">Semmle</a>
-                  </li>
-                  <li>
-                    <strong>Mar 2018 - Present</strong> - Developer Advocate @ <a href="https://semmle.com" target="_blank">Semmle</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="section">
                 <h2>Public Speaking</h2>
                 <ul>
                   <li>
-                    Jan 2017 - Noisebridge 5MOF - <a href="https://www.youtube.com/watch?v=egsswPi8yio" target="_blank">Lights &amp; Music (Video)</a>
-                  </li>
-                  <li>
-                    Oct 2018 - GitHub Universe Demo Desk - <a href="https://www.youtube.com/watch?v=aXqUWZYlFzs" target="_blank">Finding and preventing bugs with LGTM (Video)</a>
-                  </li>
-                  <li>
-                    Oct 2018 - Oracle Code One - <a href="https://oracle.rainfocus.com/widget/oracle/oow18/catalogcodeone18?search=DEV5219" target="_blank">How to Avoid 0-Days Due to Unsafe Deserialization [DEV5219] (Joint talk with Oege de Moor &amp; Bas van Schaik)</a>
-                  </li>
-                  <li>
-                    May 2019 - RVAsec - <a href="https://rvasec2019.sched.com/event/OAyv/no-more-whack-a-mole-how-to-find-and-prevent-entire-classes-of-security-vulnerabilities" target="_blank">No More Whack-a-Mole: How to Find and Prevent Entire Classes of Security Vulnerabilities</a>
-                  </li>
-                  <li>
-                    Jul 2019 - OSCON - <a href="https://www.youtube.com/watch?v=b8AHUXxGas8" target="_blank">How TypeScript is transforming the JavaScript ecosystem (Video)</a>
+                    Sep 2019 - BalCCon2k19 - <a href="https://2k19.balccon.org/speakers/273.html" target="_blank">No More Whack-a-Mole: How to Find and Prevent Entire Classes of Security Vulnerabilities</a>
                   </li>
                   <li>
                     Jul 2019 - OSCON - <a href="https://www.youtube.com/watch?v=m6Jr6w0W1xw" target="_blank">Writing npm (JavaScript) libraries using TypeScript (Video)</a>
                   </li>
                   <li>
-                    Sept 2019 - BalCCon2k19 - <a href="https://2k19.balccon.org/speakers/273.html" target="_blank">No More Whack-a-Mole: How to Find and Prevent Entire Classes of Security Vulnerabilities</a>
+                    Jul 2019 - OSCON - <a href="https://www.youtube.com/watch?v=b8AHUXxGas8" target="_blank">How TypeScript is transforming the JavaScript ecosystem (Video)</a>
+                  </li>
+                  <li>
+                    May 2019 - RVAsec - <a href="https://rvasec2019.sched.com/event/OAyv/no-more-whack-a-mole-how-to-find-and-prevent-entire-classes-of-security-vulnerabilities" target="_blank">No More Whack-a-Mole: How to Find and Prevent Entire Classes of Security Vulnerabilities</a>
+                  </li>
+                  <li>
+                    Oct 2018 - Oracle Code One - <a href="https://oracle.rainfocus.com/widget/oracle/oow18/catalogcodeone18?search=DEV5219" target="_blank">How to Avoid 0-Days Due to Unsafe Deserialization [DEV5219] (Joint talk with Oege de Moor &amp; Bas van Schaik)</a>
+                  </li>
+                  <li>
+                    Oct 2018 - GitHub Universe Demo Desk - <a href="https://www.youtube.com/watch?v=aXqUWZYlFzs" target="_blank">Finding and preventing bugs with LGTM (Video)</a>
+                  </li>
+                  <li>
+                    Jan 2017 - Noisebridge 5MOF - <a href="https://www.youtube.com/watch?v=egsswPi8yio" target="_blank">Lights &amp; Music (Video)</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="section">
+                <h2>Career</h2>
+                <ul className="bolded">
+                  <li>
+                    <strong>Mar 2018 - Present</strong> - Developer Advocate @ <a href="https://semmle.com" target="_blank">Semmle</a>
+                  </li>
+                  <li>
+                    <strong>Oct 2014 - Mar 2018</strong> - Software Engineer @ <a href="https://semmle.com" target="_blank">Semmle</a>
+                  </li>
+                  <li>
+                    <strong>Nov 2014 - Jul 2015</strong> - Computer Science Specialist Tutor @ <a href="https://www.lvs-oxford.org.uk/" target="_blank">LVS Oxford</a> - a school for young people / children on the autistic spectrum
+                  </li>
+                  <li>
+                    <strong>Feb 2012 - Feb 2014</strong> - Spent 1 year as Secretary, and 1 year as President of the Oxford University Computer Society
+                  </li>
+                  <li>
+                    <strong>2011 - 2014</strong> - Computer Science Bachelor's Degree, University of Oxford.
                   </li>
                 </ul>
               </div>
